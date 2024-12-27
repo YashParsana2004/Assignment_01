a43af8ec7c6b0c953eeae6f0231dbc002692543c
YashParsana2004 <yashparsana812@gmail.com>
2024-12-27 11:41:30 +0530

    Calendar

---
+++ Calender.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment_01
+{
+    public partial class Calender : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            Label1.Text = "Selected Date : " + Calendar1.SelectedDate.ToString();
+            Label1.Visible = true;
+        }
+    }
+}
