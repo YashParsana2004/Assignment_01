7d69e42f1bec77413154b662288ad3fae7bf7f7c
YashParsana2004 <yashparsana812@gmail.com>
2024-12-27 11:19:01 +0530

    Temperature convert

---
+++ Task_2.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Task_2.aspx.cs" Inherits="Assignment_01.Task_2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>Enter temperature:</p>
+        <asp:TextBox ID="txtTemperature" runat="server" Width="50px"></asp:TextBox>
+        <asp:RadioButtonList ID="rblUnit" runat="server" AutoPostBack="true" OnSelectedIndexChanged="rblUnit_SelectedIndexChanged">
+            <asp:ListItem Text="Fahrenheit" Value="F"></asp:ListItem>
+            <asp:ListItem Text="Celsius" Value="C"></asp:ListItem>
+        </asp:RadioButtonList>
+        <br />
+        <asp:Label ID="lblConverted" runat="server" Text="Converted Temperature:"></asp:Label>
+        <asp:Label ID="lblValue" runat="server"></asp:Label>
+        <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Task_2.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment_01
+{
+    public partial class Task_2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void rblUnit_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            double temperature = 0;
+            string unit = rblUnit.SelectedValue;
+            string convertedUnit = (unit == "F") ? "C" : "F";
+
+            if (double.TryParse(txtTemperature.Text, out temperature))
+            {
+                double convertedValue = ConvertTemperature(temperature, unit);
+                lblValue.Text = convertedValue.ToString("F2"); // Format to 2 decimal places
+                lblConverted.Text = "Converted Temperature (" + convertedUnit + "):";
+            }
+            else
+            {
+                lblValue.Text = "Invalid temperature";
+                lblConverted.Text = "";
+            }
+        }
+
+        private double ConvertTemperature(double temperature, string unit)
+        {
+            if (unit == "F")
+            {
+                return (temperature - 32) * 5 / 9;
+            }
+            else
+            {
+                return (temperature * 9 / 5) + 32;
+            }
+        }
+    }
+    }
