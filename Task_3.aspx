413122f5c9c2d7b6788b527ea2e759303ce9dd4d
YashParsana2004 <yashparsana812@gmail.com>
2024-12-27 11:25:50 +0530

    Calc

---
+++ Task_3.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Task_3.aspx.cs" Inherits="Assignment_01.Task_3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Algebraic Operations</h2>
+            <p>Enter first number:</p>
+            <asp:TextBox ID="txtFirstNumber" runat="server"></asp:TextBox>
+            <p>Enter second number:</p>
+            <asp:TextBox ID="txtSecondNumber" runat="server"></asp:TextBox>
+            <p>Select operation:</p>
+            <asp:DropDownList ID="ddlOperation" runat="server">
+                <asp:ListItem Text="Add" Value="+" />
+                <asp:ListItem Text="Subtract" Value="-" />
+                <asp:ListItem Text="Multiply" Value="*" />
+                <asp:ListItem Text="Divide" Value="/" />
+            </asp:DropDownList>
+            <br />
+            <asp:Button ID="btnCalculate" runat="server" Text="Calculate" OnClick="btnCalculate_Click" />
+            <br />
+            <p>Result:</p>
+            <asp:Label ID="lblResult" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Task_3.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment_01
+{
+    public partial class Task_3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double num1, num2, result = 0;
+            string operation = ddlOperation.SelectedValue;
+
+            if (double.TryParse(txtFirstNumber.Text, out num1) && double.TryParse(txtSecondNumber.Text, out num2))
+            {
+                switch (operation)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        if (num2 != 0)
+                        {
+                            result = num1 / num2;
+                        }
+                        else
+                        {
+                            lblResult.Text = "Division by zero is not allowed.";
+                            return;
+                        }
+                        break;
+                    default:
+                        lblResult.Text = "Invalid operation.";
+                        return;
+                }
+
+                lblResult.Text = "Result: " + result.ToString();
+            }
+            else
+            {
+                lblResult.Text = "Invalid input. Please enter valid numbers.";
+            }
+        }
+    }
+}
